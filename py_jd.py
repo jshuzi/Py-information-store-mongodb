6a9ead5d7b5b29b6f39966781a435613f2af76e5
jshuzi <jshuzi@126.com>
2020-12-07 19:33:04 +0800

    Update py_jd.py

--- py_jd.py
+++ py_jd.py
@@ -1,11 +1,15 @@
 import requests
 import pymongo
 import re
+import time
+import json
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
+    # 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:83.0) Gecko/20100101 Firefox/83.0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36',
     'Referer': 'https://item.jd.com'
 }
+
 client = pymongo.MongoClient('localhost', 27017)
 db =client['jd']
 collection = db['product']
@@ -13,17 +17,16 @@ collection = db['product']
 def find_product_id(key_word):
     jd_url = 'https://search.jd.com/Search'
     product_ids = []
-    for i in range(1,6):
-        param =
-            {
-            'keyword': key_word,
+    for i in range(0,5):
+        param = \
+            {'keyword': key_word,
              'enc':'utf-8',
              'page':i
              }
         response = requests.get(jd_url, params = param, headers=headers)
         #find all the product ids
-        ids = re.findall('data-sku="(.*?)"', response.text, re.S)
-        product_ids.append(ids)
+        ids = re.findall('<li data-sku="(.*?)"', response.text, re.S)
+        product_ids += ids
     return product_ids
 
 def get_comment_message(product_id):
@@ -34,18 +37,22 @@ def get_comment_message(product_id):
           'page={}&'\
           'pageSize=10&isShadowSku=0&fold=1'.format(product_id, i) for i in range(1, 11)]
     for url in urls:
-        response = requests.get(url, headers=headers)
-        html = response.text
-        html = html = html.replace('fetchJSON_comment98(', '').replace(');', '')
-        data = json.loads(html)
-        comments = data['comments']
-        for comment in comments:
-            product_data = {}
-            product_data['product_color'] = flush_data(comment['productColor'])
-            product_data['product_size'] = flush_data(comment['productSize'])
-            product_data['comment_content'] = comment['content']
-            product_data['create_time'] = comment['creationTime']
-            collection.insert_one(product_data)
+        try:
+            response = requests.get(url, headers=headers)
+            html = response.text
+            html = html.replace('fetchJSON_comment98(', '').replace(');', '')
+            data = json.loads(html)
+            comments = data['comments']
+            for comment in comments:
+                product_data = {}
+                product_data['product_color'] = flush_data(comment['productColor'])
+                product_data['product_size'] = flush_data(comment['productSize'])
+                product_data['comment_content'] = comment['content']
+                product_data['create_time'] = comment['creationTime']
+                collection.insert_one(product_data)
+        except:
+            time.sleep(5)
+            continue
 
 def flush_data(data):
     if '肤' in data:
@@ -78,9 +85,14 @@ def flush_data(data):
         return 'C'
     if 'D' in data:
         return 'D'
+    else:
+        return 'Else'
 
 if __name__ == '__main__':
     key_word ='胸罩'
     product_ids = find_product_id(key_word)
     for product_id in product_ids:
+        get_comment_message(product_id)
+        time.sleep(5)
+
         get_comment_message(product_id)
